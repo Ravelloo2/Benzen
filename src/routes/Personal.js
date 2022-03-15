529e7b77d6297f980e4f2c2ff0ca527f23dc6da6
kodmeron <89447350+kodmeron@users.noreply.github.com>
2022-03-15 15:01:10 +0100

    fuck-stod-p-mastern

--- src/routes/Personal.js
+++ src/routes/Personal.js
@@ -5,16 +5,18 @@ function Personal() {
         <div>
             <form>
                 <label className='fName'>Förnamn:</label><br/>
-                <input type="text" className='fName'></input><br/>
+                <input id='lName' type="text" className='fName'></input><br/>
 
                 <label className='lName'>Efternamn:</label><br/>
-                <input type="text" className='lfName'></input><br/>
+                <input id='lName' type="text" className='lfName'></input><br/>
 
                 <label className='mail'>Mail:</label><br/>
-                <input type="email" className='mail'></input><br/>
+                <input id='email' type="email" className='mail'></input><br/>
                 
-                <label className='number'>Bankkonto:</label><br/>
-                <input type="bKonto" className='bKonto'></input><br/>
+                <label id='bKonto' className='bKonto'>Bankkonto:</label><br/>
+                <input id='bKonto' type="number" className='bKonto'></input><br/>
+
+                <button className='submitBtn' id='submitBtn'>Submit</button>
             </form>
         </div>
     )
