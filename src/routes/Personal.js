3a2b6fc78bd6e9f60f9f34783c0d4eac8eed112e
kodmeron <89447350+kodmeron@users.noreply.github.com>
2022-03-15 14:23:18 +0100

    basknappar

--- src/routes/Personal.js
+++ src/routes/Personal.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
 function Personal() {
-  return (
-    <div>Personal</div>
-  )
+    return (
+        <div>
+            <form>
+                <label className='fName'>Förnamn:</label><br/>
+                <input type="text" className='fName'></input><br/>
+
+                <label className='lName'>Efternamn:</label><br/>
+                <input type="text" className='lfName'></input><br/>
+
+                <label className='mail'>Mail:</label><br/>
+                <input type="email" className='mail'></input><br/>
+                
+                <label className='number'>Bankkonto:</label><br/>
+                <input type="bKonto" className='bKonto'></input><br/>
+            </form>
+        </div>
+    )
 }
 
 export default Personal
