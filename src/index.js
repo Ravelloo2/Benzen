8ee9a1d4b93cb1aefaf87c80b2a4ffae7516b91d
Eliaz Ravello <eliaz@telia.com>
2022-03-15 10:57:20 +0100

    added bootstrap+fix routes

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
+import Ansoka from './routes/Ansoka';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Utbildningar from './routes/Utbildningar';
+import Kurser from './routes/Kurser';
+import Personal from './routes/Personal';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<App/>}/>
-      <Route path='/utbildningar' element={<App/>} />
-      <Route path='/kurser' element={<App/>} />
-      <Route path='/personal' element={<App/>} />
-      <Route path='/ansoka' element={<App/>} />
+      <Route path='/utbildningar' element={<Utbildningar/>} />
+      <Route path='/kurser' element={<Kurser/>} />
+      <Route path='/personal' element={<Personal/>} />
+      <Route path='/ansoka' element={<Ansoka/>} />
     </Routes>
     </BrowserRouter>
   </React.StrictMode>,
--- src/routes/Ansoka.js
+++ src/routes/Ansoka.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
 
-function ansoka() {
+function Ansoka() {
   return (
     <div>
       <Form>
@@ -12,19 +14,17 @@ function ansoka() {
           </Form.Text>
         </Form.Group>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">Submit</Button>
+      </Form>
     </div>
   )
 }
 
-export default ansoka
+export default Ansoka
