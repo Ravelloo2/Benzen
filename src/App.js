2a99e5ae690a77712089cab70e7e7b212f3e5daf
kodmeron <89447350+kodmeron@users.noreply.github.com>
2022-03-17 11:40:31 +0100

    ngn-stage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './css/App.css';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer'
