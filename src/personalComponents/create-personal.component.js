d9f53e6e54a26b469faf991e166ede6c0ddd0d18
kodmeron <89447350+kodmeron@users.noreply.github.com>
2022-03-22 13:30:40 +0100

    input-ksk-fix

--- src/personalComponents/create-personal.component.js
+++ src/personalComponents/create-personal.component.js
@@ -15,57 +15,59 @@ export default class CreatePersonal extends Component {
         }
     }
 
-onChangefName(e) {
-    this.setState({ fName: e.target.value })
-}
-onChangelName(e) {
-    this.setState({ lName: e.target.value })
-}
-onChangeemail(e) {
-    this.setState({ email: e.target.value })
-}
-onChangebKonto(e) {
-    this.setState({ bKonto: e.target.value })
-}
-onSubmit(e) {
-    e.preventDefault()
-    const personalObject = {
-        fName: this.state.fName,
-        lName: this.state.lName,
-        email: this.state.email,
-        bKonto: this.state.bKonto
-    };
-    axios.post('http://localhost:3001/personal', personalObject)
-    .then((res) => {
-        console.log(res.data)
-    }).catch((error) => {
-        console.log(error)
-    });
-    this.setState({ 
-        fName: '', 
-        lName: '', 
-        email: '', 
-        bKonto: ''})
-} 
+    onChangefName(e) {
+        this.setState({ fName: e.target.value })
+    }
+    onChangelName(e) {
+        this.setState({ lName: e.target.value })
+    }
+    onChangeemail(e) {
+        this.setState({ email: e.target.value })
+    }
+    onChangebKonto(e) {
+        this.setState({ bKonto: e.target.value })
+    }
+    onSubmit(e) {
+        e.preventDefault()
+        const personalObject = {
+            fName: this.state.fName,
+            lName: this.state.lName,
+            email: this.state.email,
+            bKonto: this.state.bKonto
+        };
+        axios.post('http://localhost:3001/personal', personalObject)
+            .then((res) => {
+                console.log(res.data)
+            }).catch((error) => {
+                console.log(error)
+            });
+        this.setState({
+            fName: '',
+            lName: '',
+            email: '',
+            bKonto: ''
+        })
+    }
 
-render() {
-    return (
-        <form onSubmit={this.onSubmit}>
-                <label className='fName'>Förnamn:</label><br/>
-                <input type="text" value={this.state.fName} className='fName'></input><br/>
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <label className='fName'>Förnamn:</label><br />
+                <input type="text" defaultValue={this.state.fName} onChange={this.onChangefName} className='fName'></input><br />
 
-                <label className='lName'>Efternamn:</label><br/>
-                <input type="text" value={this.state.lName} className='lfName'></input><br/>
+                <label className='lName'>Efternamn:</label><br />
+                <input type="text" defaultValue={this.state.lName} onChange={this.onChangelName} className='lfName'/><br />
 
-                <label className='email'>Mail:</label><br/>
-                <input type="email" value={this.state.email} className='email'></input><br/>
-                
-                <label className='bKonto'>Bankkonto:</label><br/>
-                <input type="number" value={this.state.bKonto} className='bKonto'></input><br/>
+                <label className='email'>Mail:</label><br />
+                <input type="text" defaultValue={this.state.email} onChange={this.onChangeemail} className='email'/><br />
 
-                <input type='submit' value="Create Personal" id='submitBtn'/>
+                <label className='bKonto'>Bankkonto:</label><br />
+                <input type="number" defaultValue={this.state.bKonto} onChange={this.onChangebKonto} className='bKonto'/><br />
+
+                <input type='submit' defaultValue="Create Personal" id='submitBtn' />
             </form>
-    )
-}
+        )
+    }
+
 }
 
