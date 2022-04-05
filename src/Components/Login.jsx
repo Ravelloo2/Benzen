98f7503adb69724d3f4b3525be7e2b32eca404d5
Eliaz Ravello <eliaz@telia.com>
2022-04-05 15:34:08 +0200

    login page done

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,35 +1,74 @@
+// Det här bara Eliaz Kod
+
 import React, { useState } from 'react'
 import '../css/login.css'
 
 const Login = () => {
-    const [user, setUser] = useState('');
-    const [password, setPassword] = useState('');
-    
-    
+    const [errorMessages, seterrorMessages] = useState({})
+    const [isSubmitted, setisSubmitted] = useState(false)
 
-    function validateForm() {
-        return user.length > 0 && password.length > 0
-    }
+    const dataBase = [
+        {
+            username: "user1",
+            password: "pass1"
+        },
+        {
+            username: "user2",
+            password: "pass2"
+        }
+    ];
 
-    function handleSubmit(event) {
+    const errors = {
+        uname: "Felaktigt användarnamn",
+        pass: "Felaktigt lösenord"
+    };
+
+    const handleSubmit = (event) => {
         event.preventDefault();
-    }
+
+        const { uname, pass } = document.forms[0];
+
+        const userData = dataBase.find((user) => user.username === uname.value);
+
+        if (userData) {
+            if (userData.password !== pass.value) {
+                seterrorMessages({ name: "pass", message: errors.pass });
+            } else {
+                setisSubmitted(true);
+            }
+        } else {
+            seterrorMessages({ name: "uname", message: errors.uname });
+        }
+    };
+
+    const renderErrorMessage = (name) => name === errorMessages.name && ( <div className='error'>{errorMessages.message}</div> );
+
+    const renderForm = (
+        <div>
+            <div className='title'>Logga In</div>
+            <form className='form-login' onSubmit={handleSubmit}>
+                    <label className='label-login'>Användarnamn</label>
+                    <input type="text" name='uname' className='user-pass' required />
+                    {renderErrorMessage("uname")}
+                    <label className='label-login'>Lösenord</label>
+                    <input type="password" name='pass' className='user-pass' required />
+                    {renderErrorMessage("pass")}
+                <div className='btn-container'>
+                    <input type="submit" className='submit-input' />
+                </div>
+            </form>
+        </div>
+    )
 
   return (
-      <div>
-          <form className='login-form' onSubmit={handleSubmit}>
-              <h2>Välkommen</h2>
-              <label className='input-label'> Användarnamn</label>
-              <input type="username" value={user} onChange={(e) => setUser(e.target.value)} className='input-login' /> 
 
-              <label className='input-label'> Lösenord </label>   
-              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className='input-login' />
-
-              <button type='submit' disabled={!validateForm()}>Logga In</button>
-          </form>
-      </div>
-  )
-
-}
-
-export default Login
+    <div className='sign-in-container'>
+         
+          <div className='login-form'>
+              
+              {isSubmitted ? <h1 className='logged-in'> Du är nu inloggad </h1> : renderForm}
+          </div></div>
+      )
+    }
+    
+    export default Login    
