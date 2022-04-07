d66c5eb3707cc26aae723b508ca06bdd60a46acf
Eliaz Ravello <eliaz@telia.com>
2022-04-07 14:47:39 +0200

    css start

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { useAuth } from './auth'
+import '../css/login.css'
 
 const Login = () => {
     const [user, setuser] = useState('')
