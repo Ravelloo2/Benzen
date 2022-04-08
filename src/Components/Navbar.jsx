a24e23982d15e57166a47589a9147aec72ae08fd
Eliaz Ravello <eliaz@telia.com>
2022-04-08 14:43:50 +0200

    login-logout function done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,34 +1,73 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { useAuth } from './auth';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "./auth";
+import LogoutPopup from "./LogoutPopup";
 
 function Navbar() {
+  const [isOpen, setisOpen] = useState(false);
+
   const auth = useAuth();
 
+  const togglePopup = () => {
+    setisOpen(!isOpen);
+  };
+
+  console.log("visa popup?", isOpen)
+
   const handleLogout = () => {
-    auth.logout(null)
-}
+    auth.logout(null);
+    setisOpen(!isOpen)
+  };
 
   return (
-    <nav className='navBar'> 
-    <div className='visible-links'></div>
-    <Link className='navBar-content' to="/utbildningar" >Utbildningar</Link> | 
-    <Link className='navBar-content' to="/Kurser" >Kurser</Link> |
-    <Link className='navBar-content' to="/personal" >Personal</Link> |
-    <Link className='navBar-content' to="/ansoka" >Ansöka</Link> |
-    <Link className='navBar-content' to="/kontakta" >Kontakta</Link> |
-    {!auth.user && (
-        <Link className='navbar-content' to="/login">
+    <nav className="navBar">
+      <div className="visible-links"></div>
+      <Link className="navBar-content" to="/utbildningar">
+        Utbildningar
+      </Link>{" "}
+      |
+      <Link className="navBar-content" to="/Kurser">
+        Kurser
+      </Link>{" "}
+      |
+      <Link className="navBar-content" to="/personal">
+        Personal
+      </Link>{" "}
+      |
+      <Link className="navBar-content" to="/ansoka">
+        Ansöka
+      </Link>{" "}
+      |
+      <Link className="navBar-content" to="/kontakta">
+        Kontakta
+      </Link>{" "}
+      |
+      {!auth.user && (
+        <Link className="navbar-content" to="/login">
           Logga in
         </Link>
       )}
-    {auth.user && (
-      <button className='logout-btn' onClick={handleLogout}>Logga ut</button>
-    )}
-  </nav>
- 
-)
-  
+      {auth.user && (
+        <div>
+          <input className="logout-btn" type="button" value="Logga ut" onClick={togglePopup} />
+          {isOpen && (
+            <LogoutPopup
+              content={
+                <div className="popup-window">
+                  <h2 className="sure">Är du Säker på att du vill logga ut?</h2>
+                  <div className="btns">
+                  <button id="yes-btn" onClick={handleLogout}>Ja</button>
+                  <button id="no-btn" onClick={togglePopup}>Nej</button>
+                  </div>
+                </div>
+              }
+              handleClose={togglePopup}
+            />
+          )}
+        </div>
+      )}
+    </nav>
+  );
 }
 
 export default Navbar;
